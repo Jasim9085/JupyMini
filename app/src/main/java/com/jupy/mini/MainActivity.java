08b9a24cb009d698c61f4b4da6fb0b2f730e5cc6
Jasim9085 <juahmed9085@gmail.com>
2025-07-31 23:24:16 +0530

    Initial Upload

---
+++ app/src/main/java/com/jupy/mini/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jupy.mini;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}```
